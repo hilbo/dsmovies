a32885fb32432b3c23912c9ec4a7b315affe719c
Hilbo Cardeira <hilbo.cardeira@gmail.com>
2022-04-06 00:12:38 -0300

    Created Page Linting

--- frontend/src/pages/form/index.tsx
+++ frontend/src/pages/form/index.tsx
@@ -1,8 +1,49 @@
 import Navbar from "components/Navbar";
+import 'pages/form/styles.css';
+import { ReactComponent as ImgFilme } from "assets/img/filme01.svg";
 
 function Form() {
+
+    const movie = {
+        id: 1,
+        image: <ImgFilme />,
+        title: "Wrath of Man",
+        count: 2,
+        score: 4.5
+    };
+
     return (
-       <p>Teste2</p>
+        <main>
+            <section className="formSection">
+                <div className="formCardFilme">
+                    
+                        {movie.image}
+                        <h3>{movie.title}</h3>
+                    
+                    <div className="formDescriptionAvaliar">
+                        
+                        <form action="" className="formulario">
+                            <div className="formularioAvaliacao">
+                                <div><label htmlFor="email">Informe seu e-mail</label></div>
+                                <div><input type="email" id="email" name="email" /></div>
+                                <div><label htmlFor="score">Informe sua avaliação</label></div>
+                                <select className="selection" name="" id="">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </select>
+                            </div>
+                            <div className="formAvaliationBtn">
+                                <button type="submit" name="Avaliar">Avaliar</button>
+                                <a href="index.html">Voltar</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 }
 export default Form;
--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,11 +1,202 @@
-import Navbar from "components/Navbar";
+import 'pages/listing/styles.css'
+import { ReactComponent as Setas } from 'assets/img/filme01.svg';
+
 
 function Listing() {
     return (
-        <div>
-            <h1>Teste</h1>
+        <div className="All">
+      
+        <div className="SelectionPageContainer container">
+            <button disabled title="Voltar" className="ButtonLeft">Imagen Seta</button>
+            <a>1 de 200999999999999</a>
+            <button title="Avançar">Imagem Seta</button>
+        </div>
+
+        <div className="row SelectionHtml">
+            
+            <div className="col-sm-6 col-lg-3 col-xl-2">
+                <div className="CardFilme">
+                    <div className="CardFilmeImg" ><Setas /></div>
+                    <div className="DescriptionBody">
+                        <div className="Description">
+                            <h3>Mulher em fúria: Produção de ação</h3>
+                            <a> Detalhameno: dfsdsdfs fdas d </a>
+                        </div>
+                        <div className="Score">
+                            <a>3,8</a>
+                            <div className="Star container">
+                                
+                            </div>
+                            <p>13 Avaliações</p>
+                            <div className="AvaliationBtn container">
+                                <a href="form.html">Avaliar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-sm-6 col-lg-3 col-xl-2">
+                <div className="CardFilme">
+                    <div className="CardFilmeImg" ><Setas /></div>
+                    <div className="DescriptionBody">
+                        <div className="Description">
+                            <h3>Mulher em fúria: Produção de ação</h3>
+                            <a> Detalhameno: dfsdsdfs fdas d </a>
+                        </div>
+                        <div className="Score">
+                            <a>3,8</a>
+                            <div className="Star container">
+                                
+                            </div>
+                            <p>13 Avaliações</p>
+                            <div className="AvaliationBtn container">
+                                <a href="form.html">Avaliar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-sm-6 col-lg-3 col-xl-2">
+                <div className="CardFilme">
+                    <div className="CardFilmeImg" ><Setas /></div>
+                    <div className="DescriptionBody">
+                        <div className="Description">
+                            <h3>Mulher em fúria: Produção de ação</h3>
+                            <a> Detalhameno: dfsdsdfs fdas d </a>
+                        </div>
+                        <div className="Score">
+                            <a>3,8</a>
+                            <div className="Star container">
+                                
+                            </div>
+                            <p>13 Avaliações</p>
+                            <div className="AvaliationBtn container">
+                                <a href="form.html">Avaliar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-sm-6 col-lg-3 col-xl-2">
+                <div className="CardFilme">
+                    <div className="CardFilmeImg" ><Setas /></div>
+                    <div className="DescriptionBody">
+                        <div className="Description">
+                            <h3>Mulher em fúria: Produção de ação</h3>
+                            <a> Detalhameno: dfsdsdfs fdas d </a>
+                        </div>
+                        <div className="Score">
+                            <a>3,8</a>
+                            <div className="Star container">
+                                
+                            </div>
+                            <p>13 Avaliações</p>
+                            <div className="AvaliationBtn container">
+                                <a href="form.html">Avaliar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-sm-6 col-lg-3 col-xl-2">
+                <div className="CardFilme">
+                    <div className="CardFilmeImg" ><Setas /></div>
+                    <div className="DescriptionBody">
+                        <div className="Description">
+                            <h3>Mulher em fúria: Produção de ação</h3>
+                            <a> Detalhameno: dfsdsdfs fdas d </a>
+                        </div>
+                        <div className="Score">
+                            <a>3,8</a>
+                            <div className="Star container">
+                                
+                            </div>
+                            <p>13 Avaliações</p>
+                            <div className="AvaliationBtn container">
+                                <a href="form.html">Avaliar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-sm-6 col-lg-3 col-xl-2">
+                <div className="CardFilme">
+                    <div className="CardFilmeImg" ><Setas /></div>
+                    <div className="DescriptionBody">
+                        <div className="Description">
+                            <h3>Mulher em fúria: Produção de ação</h3>
+                            <a> Detalhameno: dfsdsdfs fdas d </a>
+                        </div>
+                        <div className="Score">
+                            <a>3,8</a>
+                            <div className="Star container">
+                                
+                            </div>
+                            <p>13 Avaliações</p>
+                            <div className="AvaliationBtn container">
+                                <a href="form.html">Avaliar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-sm-6 col-lg-3 col-xl-2">
+                <div className="CardFilme">
+                    <div className="CardFilmeImg" ><Setas /></div>
+                    <div className="DescriptionBody">
+                        <div className="Description">
+                            <h3>Mulher em fúria: Produção de ação</h3>
+                            <a> Detalhameno: dfsdsdfs fdas d </a>
+                        </div>
+                        <div className="Score">
+                            <a>3,8</a>
+                            <div className="Star container">
+                                
+                            </div>
+                            <p>13 Avaliações</p>
+                            <div className="AvaliationBtn container">
+                                <a href="form.html">Avaliar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-sm-6 col-lg-3 col-xl-2">
+                <div className="CardFilme">
+                    <div className="CardFilmeImg" ><Setas /></div>
+                    <div className="DescriptionBody">
+                        <div className="Description">
+                            <h3>Mulher em fúria: Produção de ação</h3>
+                            <a> Detalhameno: dfsdsdfs fdas d </a>
+                        </div>
+                        <div className="Score">
+                            <a>3,8</a>
+                            <div className="Star container">
+                                
+                            </div>
+                            <p>13 Avaliações</p>
+                            <div className="AvaliationBtn container">
+                                <a href="form.html">Avaliar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        
+
         </div>
     );
+
 }
 
 export default Listing;
