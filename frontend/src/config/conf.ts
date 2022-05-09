2a2f4796e6f7fc657400f6110f6bf8e276b4404b
Hilbo Cardeira <hilbo.cardeira@gmail.com>
2022-05-08 21:58:33 -0300

    Test deploy Netilify

--- frontend/src/config/conf.ts
+++ frontend/src/config/conf.ts
@@ -1,2 +1,2 @@
-export var hostPort = 'http://192.168.50.124:8080/';
-//export var hostPort = 'https://sdmovie.herokuapp.com/';
+//export var hostPort = 'http://192.168.50.124:8080/';
+export var hostPort = 'https://sdmovie.herokuapp.com/';
